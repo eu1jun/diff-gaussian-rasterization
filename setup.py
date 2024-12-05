43a2df992a51826ccd3560f8c4e73db43112ff04
ejjng <proej11741@gmail.com>
2024-12-05 22:36:18 +0900

    :pushpin: Set ubuntu working setup

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
-#
-# Copyright (C) 2023, Inria
-# GRAPHDECO research group, https://team.inria.fr/graphdeco
-# All rights reserved.
-#
-# This software is free for non-commercial, research and evaluation use 
-# under the terms of the LICENSE.md file.
-#
-# For inquiries contact  george.drettakis@inria.fr
-#
-
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
-os.path.dirname(os.path.abspath(__file__))
+
+# Get absolute path to the glm directory in the local-libs
+glm_local_path = os.path.expanduser('~/glm')
 
 setup(
     name="diff_gaussian_rasterization",
@@ -21,13 +12,19 @@ setup(
         CUDAExtension(
             name="diff_gaussian_rasterization._C",
             sources=[
-            "cuda_rasterizer/rasterizer_impl.cu",
-            "cuda_rasterizer/forward.cu",
-            "cuda_rasterizer/backward.cu",
-            "rasterize_points.cu",
-            "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
-        ],
+                "cuda_rasterizer/rasterizer_impl.cu",
+                "cuda_rasterizer/forward.cu",
+                "cuda_rasterizer/backward.cu",
+                "rasterize_points.cu",
+                "ext.cpp"],
+            extra_compile_args={
+                "cxx": ["-std=c++17"],
+                "nvcc": [
+                    "-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/"),
+                    "-I" + glm_local_path,  # Use expanded home directory
+                ]
+            })
+    ],
     cmdclass={
         'build_ext': BuildExtension
     }
